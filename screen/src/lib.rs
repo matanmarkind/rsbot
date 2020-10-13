514da793016ee13f191a168c2f0d745753675509
Matan Markind <matan.markind@gmail.com>
2020-10-13 21:59:08 +0300

    Move screen Capturer to frame.rs.
    Capturer is a fairly thin class for getting frames. Didn't seem to really deserve having it's own file.

--- screen/src/lib.rs
+++ screen/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod action_letters;
-pub mod capture;
 pub mod constants;
 pub mod frame;
 pub mod inventory;
@@ -8,7 +7,6 @@ pub mod types;
 
 /// Publish internals so users can use screen::Frame instead of
 /// screen::frame::Frame.
-pub use capture::*;
 pub use constants::*;
 pub use frame::*;
 pub use locations::Locations;
